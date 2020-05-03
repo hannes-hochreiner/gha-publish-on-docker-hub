675f2ffe6867424e8f0935504e30a23c6d816041
Hannes Hochreiner <hannes@hochreiner.net>
2020-05-03 18:38:30 +0200

    added working code

--- publish-on-docker-hub/src/index.js
+++ publish-on-docker-hub/src/index.js
@@ -1,12 +1,21 @@
 const core = require('@actions/core');
 
-try {
-  const dockerUserName = core.getInput('docker-user-name');
-  const dockerToken = core.getInput('docker-token');
-  console.log(`docker-user-name: ${dockerUserName}`);
-  console.log(`docker-token: ${dockerToken}`);
-  console.log(`GITHUB_REF: ${process.env.GITHUB_REF}`);
-  console.log(`GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function init() {
+  try {
+    const dockerUserName = core.getInput('docker-user-name');
+    const dockerToken = core.getInput('docker-token');
+    const dockerTag = process.env.GITHUB_REF.split('/')[2];
+    const dockerRepo = process.env.GITHUB_REPOSITORY.split('/')[1];
+    console.log(`GITHUB_REF: ${process.env.GITHUB_REF}`);
+    console.log(`GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}`);
+    const dockerFullName = `${dockerUserName}/${dockerRepo}:${dockerTag}`;
+
+    await exec.exec('docker', ['build', '-t', dockerFullName]);
+    await exec.exec('docker', ['login', '-u', dockerUserName, '-p', dockerToken]);
+    await exec.exec('docker', ['push', dockerFullName]);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+init();
