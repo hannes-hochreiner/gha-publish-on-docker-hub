592b62d06eadcbdbb77c9344066af5f171ff94f8
Hannes Hochreiner <hannes@hochreiner.net>
2020-05-03 18:05:24 +0200

    fixed env bug

--- publish-on-docker-hub/src/index.js
+++ publish-on-docker-hub/src/index.js
@@ -5,8 +5,8 @@ try {
   const dockerToken = core.getInput('docker-token');
   console.log(`docker-user-name: ${dockerUserName}`);
   console.log(`docker-token: ${dockerToken}`);
-  console.log(`GITHUB_REF: ${GITHUB_REF}`);
-  console.log(`GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}`);
+  console.log(`GITHUB_REF: ${process.env.GITHUB_REF}`);
+  console.log(`GITHUB_REPOSITORY: ${process.env.GITHUB_REPOSITORY}`);
 } catch (error) {
   core.setFailed(error.message);
 }
